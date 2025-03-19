0b00dbed52c98dc5ce598e0bb083a6dc2e1c8b8f
Erwin van der Valk <erwin@vandervalk.pro>
2025-03-19 14:31:21 +0100

    added quickstart

---
+++ BFF/v3/Quickstarts/BlazorBffApp/BlazorBffApp.Client/Program.cs
@@ -0,0 +1,11 @@
+using Duende.Bff.Blazor.Client;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services
+    .AddBffBlazorClient(); // Provides auth state provider that polls the /bff/user endpoint
+
+builder.Services
+    .AddCascadingAuthenticationState();
+await builder.Build().RunAsync();
