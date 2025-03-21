b7b6d44b4501fe02482a4da7a4376250b1d2ab9b
Maarten Balliauw <maarten@maartenballiauw.be>
2025-03-21 21:34:26 +0100

    Run dotnet format

--- IdentityServer/v6/Basics/Apis/SimpleApi/Program.cs
+++ IdentityServer/v6/Basics/Apis/SimpleApi/Program.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Duende Software. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+
 using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
--- IdentityServer/v6/Basics/Apis/SimpleApi/Startup.cs
+++ IdentityServer/v6/Basics/Apis/SimpleApi/Startup.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Duende Software. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+
 using System.IdentityModel.Tokens.Jwt;
 using Client;
 using Microsoft.AspNetCore.Builder;
