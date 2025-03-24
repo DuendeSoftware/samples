996405d3d9c5e53b692e3bab5a9da6917581dd4a
khalidabuhakmeh <khalidabuhakmeh@gmail.com>
2025-03-24 09:25:35 -0400

    Improve authentication button handling in Swagger UI
    Refactor logic to dynamically add authentication buttons only when not already present. Introduce a more reliable method to detect and handle the rendering of the Swagger UI elements using MutationObserver with additional safeguards. This ensures improved performance and stability in handling authentication states.

--- BFF/v3/OpenApi/OpenApi.Bff/wwwroot/swagger/bff-auth-button.js
+++ BFF/v3/OpenApi/OpenApi.Bff/wwwroot/swagger/bff-auth-button.js
@@ -1,52 +1,68 @@
 async function checkAuthentication() {
   try {
-    let response = await fetch('/bff/user', { credentials: 'include', headers: { "x-csrf": 1 } });
-
-    let wrapper = document.querySelector('.schemes.wrapper');
-
-    wrapper.insertAdjacentHTML('beforeend', `<div class='auth-wrapper'><button class='btn authorize unlocked btn-success'></button></div>`);
-
+    // check if we already added it
     let authorizeButton = document.querySelector('.swagger-ui .auth-wrapper .btn');
-    if (!authorizeButton) return;
+    if (authorizeButton === null) {
 
-    if (response.status === 401) {
-      // User is unauthenticated
-      authorizeButton.innerText = "Log in";
-      authorizeButton.onclick = function (event) {
-        event.preventDefault();
-        window.location.href = "/bff/login?returnUrl=" + window.location.pathname;
-      };
+      let response = await fetch('/bff/user',
+        {
+          credentials: 'include',
+          headers: {'x-csrf': '1'}
+        });
 
-    } else if (response.status === 200) {
+      let wrapper = document.querySelector('.schemes.wrapper');
+      wrapper.insertAdjacentHTML('beforeend', `<div class='auth-wrapper'><button class='btn authorize unlocked btn-success'></button></div>`);
+      authorizeButton = document.querySelector('.swagger-ui .auth-wrapper .btn');
 
-      let claims = await response.json();
-
-      let logoutUrlClaim = claims.find(claim => claim.type === 'bff:logout_url');
-      if (logoutUrlClaim) {
+      if (response.status === 401) {
+        // User is unauthenticated
+        authorizeButton.innerText = "Log in";
         authorizeButton.onclick = function (event) {
           event.preventDefault();
-          window.location.href = `${logoutUrlClaim.value}&returnUrl=${window.location.pathname}`;
+          window.location.href = "/bff/login?returnUrl=" + window.location.pathname;
         };
-      }
-      // User is authenticated
-      authorizeButton.innerText = "Log out";
-      authorizeButton.classList.add("btn-success"); // Optional: Change button style
 
+      } else if (response.status === 200) {
+        let claims = await response.json();
+        let logoutUrlClaim = claims.find(claim => claim.type === 'bff:logout_url');
+        if (logoutUrlClaim) {
+          authorizeButton.onclick = function (event) {
+            event.preventDefault();
+            window.location.href = `${logoutUrlClaim.value}&returnUrl=${window.location.pathname}`;
+          };
+        }
+        // User is authenticated
+        authorizeButton.innerText = "Log out";
+        authorizeButton.classList.add("btn-success");
+      }
     }
-  } catch (error) {
+  } catch
+    (error) {
     console.error("Error checking authentication:", error);
   }
 }
 
-const observer = new MutationObserver((mutations, obs) => {
-  const wrapper = document.querySelector('.schemes.wrapper');
-  if (wrapper) {
-    checkAuthentication();
-    obs.disconnect();
-  }
+document.addEventListener("DOMContentLoaded", e => {
+  // watch the only element that's on the page when the page loads
+  const swaggerUi = document.querySelector('#swagger-ui');
+
+  const observer = new MutationObserver(async (mutations, obs) => {
+    let schemeContainer = document.querySelector('.scheme-container');
+    const intervalId = setInterval(() => {
+      schemeContainer = document.querySelector('.scheme-container');
+      if (schemeContainer) {
+        clearInterval(intervalId);
+        checkAuthentication();
+      }
+    }, 100);
+  });
+
+  observer.observe(swaggerUi, {
+    childList: true,
+    subtree: true
+  });
 });
 
-observer.observe(document, {
-  childList: true,
-  subtree: true
-});
+
+
+
