d16964e4e76ea6367e6ffea84253fa68deff8c90
Anders Abel <anders@duendesoftware.com>
2025-04-30 11:05:16 +0200

    Make it work both from the command line and when run from Visual Studio

--- IdentityServer/v7/MTLS/ClientCredentials/Program.cs
+++ IdentityServer/v7/MTLS/ClientCredentials/Program.cs
@@ -70,7 +70,13 @@ public class Program
     {
         var handler = new SocketsHttpHandler();
 
-        var cert = new X509Certificate2("../localhost-client.p12", "changeit");
+        // When running from Visual Studio the current directory gets set to the assembly
+        // location, but when running from command prompt with dotnet run the current
+        // directory is likely the project directory. This works for both.
+        var assemblyDir = typeof(Program).Assembly.Location;
+        var certPath = Path.GetFullPath(Path.Combine(assemblyDir, "../../../../../localhost-client.p12"));
+
+        var cert = new X509Certificate2(certPath, "changeit");
         handler.SslOptions.ClientCertificates = new X509CertificateCollection { cert };
 
         return handler;
