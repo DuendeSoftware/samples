6f119458d5dcf2786a1c6e2131e302576b6581be
Wesley Cabus <wesley@gotsharp.be>
2025-11-17 11:49:56 +0100

    Updated Maui sample to .NET 9 (will do a separate .NET 10 version later)

--- various/clients/Maui/MauiApp1/MauiApp1/App.xaml.cs
+++ various/clients/Maui/MauiApp1/MauiApp1/App.xaml.cs
@@ -1,11 +1,9 @@
-﻿namespace MauiApp1;
+
+namespace MauiApp1;
 
 public partial class App : Application
 {
-    public App()
-    {
-        InitializeComponent();
+    public App() => InitializeComponent();
 
-        MainPage = new AppShell();
-    }
+    protected override Window CreateWindow(IActivationState? activationState) => new Window(new AppShell());
 }
