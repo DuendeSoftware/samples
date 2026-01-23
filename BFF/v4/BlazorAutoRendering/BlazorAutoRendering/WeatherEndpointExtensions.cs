1ecb47a81bef177179e205e307d828d80baf9bf9
RolandGuijt <github@4sh.nl>
2026-01-23 16:11:05 +0100

    Merge pull request #291
    * Add BFF 4 autorendering sample
    
    * Update slnx

---
+++ BFF/v4/BlazorAutoRendering/BlazorAutoRendering/WeatherEndpointExtensions.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Duende Software. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Duende.Bff;
+
+namespace BlazorAutoRendering;
+
+public static class WeatherEndpointExtensions
+{
+    public static void MapWeatherEndpoints(this WebApplication app)
+    {
+        app.MapGet("/WeatherForecast", async (IWeatherClient weatherClient) => await weatherClient.GetWeatherForecasts())
+            .RequireAuthorization()
+            .AsBffApiEndpoint();
+    }
+
+
+}
