112267f0fcb56735d7525725a49cad1b9e930d18
Roland Guijt <roland.guijt@duendesoftware.com>
2026-04-22 12:23:04 +0200

    Aspire for PAT samples

--- IdentityServer/v8/PAT/IdentityServerHost/Program.cs
+++ IdentityServer/v8/PAT/IdentityServerHost/Program.cs
@@ -3,39 +3,13 @@
 
 using IdentityServerHost;
 using Microsoft.AspNetCore.DataProtection;
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
-
-Console.Title = "IdentityServerHost";
-
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
-    .MinimumLevel.Override("System", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-    .Enrich.FromLogContext()
-    // uncomment to write to Azure diagnostics stream
-    //.WriteTo.File(
-    //    @"D:\home\LogFiles\Application\IdentityServerHost.txt",
-    //    fileSizeLimitBytes: 1_000_000,
-    //    rollOnFileSizeLimit: true,
-    //    shared: true,
-    //    flushToDiskInterval: TimeSpan.FromSeconds(1))
-    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
-    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSerilog();
+builder.AddServiceDefaults();
 builder.Services.AddRazorPages();
 
 var idsvrBuilder = builder.Services.AddIdentityServer(options =>
 {
-    options.Events.RaiseErrorEvents = true;
-    options.Events.RaiseInformationEvents = true;
-    options.Events.RaiseFailureEvents = true;
-    options.Events.RaiseSuccessEvents = true;
 })
     .AddTestUsers(TestUsers.Users);
 
@@ -51,6 +25,8 @@ builder.Services.AddDataProtection()
     .SetApplicationName("IdentityServer");
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
