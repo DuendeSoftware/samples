bf93cbf3b472d6aed9517bb21af6cd87da179351
RolandGuijt <github@4sh.nl>
2026-04-28 14:41:15 +0200

    Aspire for PipelineRegistration sample (#339)
    * Aspire for PipelineRegistration sample
    
    * Remove serilog from simple api
    
    ---------
    
    Co-authored-by: Roland Guijt <roland.guijt@duendesoftware.com>

--- IdentityServer/v8/Apis/SimpleApi/Program.cs
+++ IdentityServer/v8/Apis/SimpleApi/Program.cs
@@ -1,25 +1,9 @@
 // Copyright (c) Duende Software. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
-
-Console.Title = "Simple API";
-Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                .MinimumLevel.Override("System", LogEventLevel.Information)
-                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
-                .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Code)
-                .CreateLogger();
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-
-builder.Services.AddSerilog();
 
 builder.Services.AddControllers();
 
