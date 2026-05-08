cc7341e54230c1bb184e48e8bb4de150cbb46f03
Roland Guijt <roland.guijt@duendesoftware.com>
2026-05-08 09:44:14 +0200

    Aspire for AspNetIdentityPasskeys sample + include a client

--- IdentityServer/v8/AspNetIdentityPasskeys/IdentityServerAspNetIdentityPasskeys/HostingExtensions.cs
+++ IdentityServer/v8/AspNetIdentityPasskeys/IdentityServerAspNetIdentityPasskeys/HostingExtensions.cs
@@ -10,49 +10,11 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Serilog;
-using Serilog.Filters;
 
 namespace IdentityServerAspNetIdentityPasskeys;
 
 internal static class HostingExtensions
 {
-    public static WebApplicationBuilder ConfigureLogging(this WebApplicationBuilder builder)
-    {
-        // Write most logs to the console but diagnostic data to a file.
-        // See https://docs.duendesoftware.com/identityserver/diagnostics/data
-        builder.Host.UseSerilog((ctx, lc) =>
-        {
-            lc.WriteTo.Logger(consoleLogger =>
-            {
-                consoleLogger.WriteTo.Console(
-                    outputTemplate:
-                    "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
-                    formatProvider: CultureInfo.InvariantCulture);
-                if (builder.Environment.IsDevelopment())
-                {
-                    consoleLogger.Filter.ByExcluding(Matching.FromSource("Duende.IdentityServer.Diagnostics.Summary"));
-                }
-            });
-            if (builder.Environment.IsDevelopment())
-            {
-                lc.WriteTo.Logger(fileLogger =>
-                {
-                    fileLogger
-                        .WriteTo.File("./diagnostics/diagnostic.log", rollingInterval: RollingInterval.Day,
-                            fileSizeLimitBytes: 1024 * 1024 * 10, // 10 MB
-                            rollOnFileSizeLimit: true,
-                            outputTemplate:
-                            "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
-                            formatProvider: CultureInfo.InvariantCulture)
-                        .Filter
-                        .ByIncludingOnly(Matching.FromSource("Duende.IdentityServer.Diagnostics.Summary"));
-                }).Enrich.FromLogContext().ReadFrom.Configuration(ctx.Configuration);
-            }
-        });
-        return builder;
-    }
-
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
         builder.Services.AddRazorPages();
@@ -84,12 +46,6 @@ internal static class HostingExtensions
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
-
-                // Use a large chunk size for diagnostic data in development where it will be redirected to a local file.
-                if (builder.Environment.IsDevelopment())
-                {
-                    options.Diagnostics.ChunkSize = 1024 * 1024 * 10; // 10 MB
-                }
             })
             .AddInMemoryIdentityResources(Config.IdentityResources)
             .AddInMemoryApiScopes(Config.ApiScopes)
@@ -121,7 +77,7 @@ internal static class HostingExtensions
 
     public static WebApplication ConfigurePipeline(this WebApplication app)
     {
-        app.UseSerilogRequestLogging();
+        app.MapDefaultEndpoints();
 
         if (app.Environment.IsDevelopment())
         {
