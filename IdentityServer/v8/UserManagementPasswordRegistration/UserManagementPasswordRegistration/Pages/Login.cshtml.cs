162117e94d2a36249aa35d8b81dc11bdcf8ed64a
AL Rodriguez <al.rodriguez@duendesoftware.com>
2026-06-16 14:38:05 -0400

    Fixes from Copilot suggestions

--- IdentityServer/v8/UserManagementPasswordRegistration/UserManagementPasswordRegistration/Pages/Login.cshtml.cs
+++ IdentityServer/v8/UserManagementPasswordRegistration/UserManagementPasswordRegistration/Pages/Login.cshtml.cs
@@ -8,6 +8,8 @@ using Duende.IdentityModel;
 using Duende.IdentityServer;
 using Duende.UserManagement;
 using Duende.UserManagement.Authentication.Passwords;
+using Duende.UserManagement.Profiles;
+
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
