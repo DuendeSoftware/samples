9d1a90785dfbf826c05aa269ed3ae1d40d796d51
Wesley Cabus <wesley@gotsharp.be>
2026-08-06 11:09:00 +0200

    Formatting

--- IdentityServer/v7/SessionMigration/MigratingTicketDataFormat.cs
+++ IdentityServer/v7/SessionMigration/MigratingTicketDataFormat.cs
@@ -10,13 +10,13 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 namespace SessionMigration;
 public class MigratingTicketDataFormat : ISecureDataFormat<AuthenticationTicket>
 {
-    private readonly IHttpContextAccessor httpContextAccessor;
-    private readonly ISecureDataFormat<AuthenticationTicket> inner;
-    private CookieAuthenticationOptions options;
-    private readonly string scheme;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ISecureDataFormat<AuthenticationTicket> _inner;
+    private readonly CookieAuthenticationOptions _options;
+    private readonly string _scheme;
 
-    // Copied from Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationHandler,
-    // unfortunatley it's private and cannot be referenced.
+    // Copied from Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationHandler.
+    // Unfortunately, it's private and cannot be referenced.
     private const string SessionIdClaim = "Microsoft.AspNetCore.Authentication.Cookies-SessionId";
 
     public MigratingTicketDataFormat(
@@ -24,21 +24,21 @@ public class MigratingTicketDataFormat : ISecureDataFormat<AuthenticationTicket>
         CookieAuthenticationOptions options,
         string scheme)
     {
-        this.httpContextAccessor = httpContextAccessor;
-        this.options = options;
-        this.scheme = scheme;
+        _httpContextAccessor = httpContextAccessor;
+        _options = options;
+        _scheme = scheme;
 
         // Capture the inner at construction as the value in options will be replaced with
         // a reference to this instance.
-        inner = options.TicketDataFormat;
+        _inner = options.TicketDataFormat;
     }
 
-    public string Protect(AuthenticationTicket data) => inner.Protect(data);
-    public string Protect(AuthenticationTicket data, string purpose) => inner.Protect(data, purpose);
-    public AuthenticationTicket Unprotect(string protectedText) => inner.Unprotect(protectedText);
+    public string Protect(AuthenticationTicket data) => _inner.Protect(data);
+    public string Protect(AuthenticationTicket data, string purpose) => _inner.Protect(data, purpose);
+    public AuthenticationTicket Unprotect(string protectedText) => _inner.Unprotect(protectedText);
     public AuthenticationTicket Unprotect(string protectedText, string purpose)
     {
-        var ticket = inner.Unprotect(protectedText, purpose);
+        var ticket = _inner.Unprotect(protectedText, purpose);
 
         if (ticket.Principal.HasClaim(c => c.Type == SessionIdClaim))
         {
@@ -46,7 +46,7 @@ public class MigratingTicketDataFormat : ISecureDataFormat<AuthenticationTicket>
             return ticket;
         }
 
-        var context = httpContextAccessor.HttpContext;
+        var context = _httpContextAccessor.HttpContext;
         var sessionStore = context.RequestServices.GetRequiredService<IServerSideTicketStore>();
 
         // Unprotect isn't async so we'll have block the thread and wait using .Result when calling an
@@ -66,18 +66,18 @@ public class MigratingTicketDataFormat : ISecureDataFormat<AuthenticationTicket>
         {
             var principal = new ClaimsPrincipal(
                 new ClaimsIdentity(
-                    new[] { new Claim(SessionIdClaim, sessionId, ClaimValueTypes.String, options.ClaimsIssuer) },
-                    options.ClaimsIssuer));
+                    new[] { new Claim(SessionIdClaim, sessionId, ClaimValueTypes.String, _options.ClaimsIssuer) },
+                    _options.ClaimsIssuer));
 
             ticket = new AuthenticationTicket(principal, null, scheme);
 
-            var cookieValue = inner.Protect(ticket, purpose);
+            var cookieValue = _inner.Protect(ticket, purpose);
 
             var cookieOptions = CreateCookieOptions(ticket, context);
 
-            options.CookieManager.AppendResponseCookie(
+            _options.CookieManager.AppendResponseCookie(
                 context,
-                options.Cookie.Name!,
+                _options.Cookie.Name!,
                 cookieValue,
                 cookieOptions);
         }
@@ -88,7 +88,7 @@ public class MigratingTicketDataFormat : ISecureDataFormat<AuthenticationTicket>
     {
         // Cookie option generation copied from cookie handler.
 
-        var cookieOptions = options.Cookie.Build(context);
+        var cookieOptions = _options.Cookie.Build(context);
         cookieOptions.Expires = null;
 
         if (ticket.Properties.IsPersistent)
